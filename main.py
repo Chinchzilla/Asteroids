8826348454303d153c7176fc861b54c0c6f246c6
Chinchzilla <git@chinchzilla.slmail.me>
2025-12-03 19:49:58 +0100

    add player

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -18,9 +20,13 @@ def main():
                 pygame.quit()
                 return
 
+        player.update(dt)
+
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
+        # limit the framerate to 60 FPS
         dt = clock.tick(60) / 1000
 
 
